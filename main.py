519cb9ea54fda15333376281a273d5cc82872138
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-04-18 19:19:44 +0000

    added embed and customized messages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import asyncio
 
 client = discord.Client()
 
@@ -9,12 +10,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  #ignore message from bot
   if message.author == client.user:
     return
-  if message.content.startswith('!start'):
-    await message.channel.send('Welcome to SpyBot!')
+  if message.content.startswith('!assassin'):
+    embedIT = discord.Embed(description="Entering kill mode....", color=0x00ff00)
+    await message.channel.send(embed=embedIT)
+    #wait 5 seconds for next message
+    await asyncio.sleep(5)
+    await message.channel.send("""There are assassins running loose! Find them before it's too late""")
 
-client.run(os.environ['TOKEN'])
+
+#prevent 
+try:
+  client.run(os.environ['TOKENS'])
+except:
+    os.system("kill 1")
   
   
 

a6a2e54c6257c8f58b257789ea2b4613923a35da
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-04-18 03:21:21 +0000

    imported discord library

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("Hey it's me {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('!hello'):
+    await message.channel.send('Hello!')
+
+  
+  
+
